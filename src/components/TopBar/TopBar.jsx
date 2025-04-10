427d05b6a00d4c2bd1354f08c963a3b8ebcacf75
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-04-10 22:16:14 +0800

    Fourth Version

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './topBar.css'
 
 const TopBar = (props) => {
   const {setPages} = props
+
+  const [projborder, setprojborder] = useState(false)
+  const [aboutborder, setaboutborder] = useState(false)
+
+
   return (
     <div className="top-bar-cont">
-      <div className="logo-cont">jeg</div>
+      <div className="logo-cont">
+        <img src="/goi logo.png" alt="" />
+        <p>JPDG.goi</p>
+      </div>
       <div className="nav-button-cont">
         <div onClick={
           () => {
             setPages(true)
+            setaboutborder(true)
+            setprojborder(false)
           }
-        }>ABOUT</div>
+        } className={aboutborder ? "buttborder" : " "}>ABOUT</div>
         <div onClick={
           () => {
             setPages(false)
+            setprojborder(true)
+            setaboutborder(false)
           }
-        }>PROJECTS</div>
+        } className={projborder ? "buttborder":" "}>PROJECTS</div>
       </div>
-      <div>empty</div>
+      <div></div>
     </div>
   )
 }
